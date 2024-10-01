428fe474a10390a18328708fa8b65a375d5cf79b
Henrik Soerensen <henrik@voi.network>
2024-10-01 10:19:41 -0400

    Add banner on how to get Voi

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import AppRouter from "./AppRouter";
 import {useDispatch} from "react-redux";
 import {initApp} from "../../redux/app/actions/app";
 import { useLocation } from 'react-router-dom';
+import Header from '../Modules/Header/Header';
 
 function App(): JSX.Element {
     const dispatch = useDispatch();
@@ -14,6 +15,7 @@ function App(): JSX.Element {
 
   return (
       <div className="app-root">
+            <Header></Header>
           <AppRouter></AppRouter>
       </div>
   );
---
+++ src/components/Modules/Header/Header.tsx
@@ -0,0 +1,20 @@
+// src/components/Header.tsx
+import React from 'react';
+import './Header.scss';
+
+const Header: React.FC = () => {
+    return (
+        <header className="app-header">
+            <h1>Looking for ways to get Voi?</h1>
+            <nav>
+                <ul>
+                    <li><a href="https://www.mexc.com/exchange/VOI_USDT">MEXC</a></li>
+                    <li><a href="https://www.coinstore.com/spot/VOIUSDT">Coinstore</a></li>
+                    <li><a href="https://medium.com/@voifoundation/staking-program-how-to-guide-382ea5085dab">DEX Guide</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
