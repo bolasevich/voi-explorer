2c0af227ce91fb22409e1ca947456340eccce5c9
Henrik Soerensen <henrik@voi.network>
2024-10-01 12:00:10 -0400

    Remove header item

--- src/components/Modules/Header/Header.tsx
+++ src/components/Modules/Header/Header.tsx
@@ -5,7 +5,6 @@ import './Header.scss';
 const Header: React.FC = () => {
     return (
         <header className="app-header">
-            <h1>Get Voi</h1>
             <nav>
                 <ul>
                     <li><a href="https://www.mexc.com/exchange/VOI_USDT">MEXC</a></li>
