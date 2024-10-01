f0fa3aa8ad5102b38a7f93928ff5d9a45e53f8b5
Henrik Soerensen <henrik@voi.network>
2024-10-01 12:15:15 -0400

    Left align nav links

--- src/components/Modules/Header/Header.tsx
+++ src/components/Modules/Header/Header.tsx
@@ -5,6 +5,7 @@ import './Header.scss';
 const Header: React.FC = () => {
     return (
         <header className="app-header">
+            <h1>Want to get Voi?</h1>
             <nav>
                 <ul>
                     <li><a href="https://www.mexc.com/exchange/VOI_USDT">MEXC</a></li>
